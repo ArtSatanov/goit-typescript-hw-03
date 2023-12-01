6495493e8f45eef3cba96a13a2211b354fdd115a
ArtSatanov <artsatanov@gmail.com>
2023-12-01 02:54:44 +0200

    4

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,46 @@
+class Key {
+   private signature: number;
+   constructor() {
+      this.signature = Math.random();
+   }
+   getSignature(): number {
+       return this.signature;
+      }
+};
+
+class Person {
+   constructor(private key: Key) { }
+   getKey(): Key {
+      return this.key;
+   }
+}
+
+abstract class House {
+   protected door: boolean = false;
+   protected tenants: Person[] = [];
+   constructor(protected key: Key) { }
+   public comeIn(person: Person) {
+      if (this.door !== true) {
+         console.log("GoodBye!")
+         return;
+      }
+      this.tenants.push(person);
+   }
+   abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+   openDoor(key: Key): void {
+      if (key.getSignature() !== this.key.getSignature()) {
+         console.log("GoodBye!");
+         return;
+      }
+      this.door = true;
+      console.log("Come in!");
+   }
+}
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
